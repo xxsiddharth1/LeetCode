8cf51155f334e5bed2a7675d3207fd3048fabf41
xxsiddharth1 <108428699+xxsiddharth1@users.noreply.github.com>
2023-06-10 23:46:44 +0530

    Add files via upload

---
+++ Feb02.java
@@ -0,0 +1,45 @@
+package com.feb;
+
+//https://leetcode.com/problems/verifying-an-alien-dictionary/
+public class Feb02 {
+    public static void main(String[] args) {
+        String[] words = {"hello","leetcode"};
+        String order = "hlabcdefgijkmnopqrstuvwxyz";
+
+        System.out.println(isAlienSorted(words, order));
+    }
+
+    static int[] char_map;
+    static boolean isAlienSorted(String[] words, String order) {
+        char_map = new int[26];
+        for(int i =0; i < order.length(); i++){
+            char_map[order.charAt(i) - 'a'] = i;
+        }
+
+        for(int i = 1; i < words.length; i++){
+            if(compare(words[i-1], words[i]) > 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static int compare(String word1, String word2){
+        int i = 0;
+        int j = 0;
+        int char_compare_val = 0;
+
+        while( i< word1.length() && j < word2.length() && char_compare_val == 0){
+            char_compare_val = char_map[word1.charAt(i)-'a'] - char_map[word2.charAt(j)- 'a'];
+            i++;
+            j++;
+        }
+
+        if(char_compare_val == 0){
+            return word1.length() - word2.length();
+        } else{
+            return char_compare_val;
+        }
+    }
+}
