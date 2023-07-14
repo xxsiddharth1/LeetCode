e74242a0bcdd4d21fc91775049430755aaa0061b
xxsiddharth1 <108428699+xxsiddharth1@users.noreply.github.com>
2023-07-14 15:53:33 +0530

    Add files via upload

---
+++ pattern28.java
@@ -0,0 +1,20 @@
+package Patterns;
+
+public class pattern28 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int row = 0; row < 2 * n; row++) {
+            int totalColsInRow = row > n ? 2 * n - row: row;
+
+            int noOfSpaces = n - totalColsInRow;
+            for (int s = 0; s < noOfSpaces; s++) {
+                System.out.print(" ");
+            }
+
+            for (int col = 0; col < totalColsInRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
