5f5b4403546801c8eb8c16a5f016c0398580d176
xxsiddharth1 <siddharth2015sharma@gmail.com>
2023-08-22 20:12:20 +0530

    DSA

---
+++ array1.java
@@ -0,0 +1,16 @@
+public class arrayfrompermutation {
+    static int[] buildArray(int[] nums) {
+      int n = nums.length;
+      int[] ans = new int[n];
+      for(int i = 0; i <n;i++){
+          ans[i] = nums[nums[i]];
+      }
+      return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0,2,1,5,3,4};
+        int [] ans = buildArray(nums);
+        System.out.println(ans);
+    }
+}
