b3330b5ab679d23a804b745979ec1fde9717db6b
xxsiddharth1 <siddharth2015sharma@gmail.com>
2023-09-02 20:12:20 +0530

    DSA

---
+++ func1.java
@@ -0,0 +1,16 @@
+public class do_while {
+    public static void main(String[] args) {
+        int b = 0;
+        do{
+            System.out.println(b);
+            b++;
+        } while(b<5);
+//
+//        while(b<5){
+//            System.out.println(b);
+//            b++;
+//        }
+
+
+    }
+}
