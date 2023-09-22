a1dbde838ba13f61810fb85f7ada4c1130731e66
xxsiddharth1 <siddharth2015sharma@gmail.com>
2023-09-22 20:12:20 +0530

    DSA

---
+++ Array_is_sorted1.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class Array_is_sorted {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8};
+        System.out.println(sorted(arr,0));
+    }
+    static boolean sorted(int[] arr, int index){
+        if(arr[index]==arr.length-1){
+            return true;
+        }
+        return arr[index] < arr[index+1] && sorted(arr,index+1);
+    }
+}
