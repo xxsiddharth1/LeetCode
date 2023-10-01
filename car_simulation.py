8be45dfc3673a874bf641c61e281a04986b4d40b
xxsiddharth1 <siddharth2015sharma@gmail.com>
2023-10-01 20:12:20 +0530

    DSA

---
+++ car_simulation.py
@@ -0,0 +1,10 @@
+#this is a car simulation game in which if the user types starts then the cars starts, if we type stop the car will stop and if we type quit the game will exit 
+input = input("type 'start' to start the game! ")
+if input == 'start':
+    print('the game has started')
+elif input == 'stop':
+    print('the game has stopped')
+elif input == 'pause':
+    print('game paused')
+    
+   
