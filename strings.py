65d2f6585ec9402f21922f25fe6509a6143628fc
xxsiddharth1 <siddharth2015sharma@gmail.com>
2023-10-14 20:12:20 +0530

    DSA

---
+++ strings.py
@@ -0,0 +1,14 @@
+string1 = "We can use double qoutes for apostriphie 's "
+print(string1)
+string2 = ''' A
+multi
+line
+string
+'''
+print(string2)
+string3 = 'Siddharth Sharma'
+print(string3[0])
+string4 = 'Siddharth Sharma'
+print(string4[-1])
+string5 = 'Siddharth sharma'
+print(string5[0:3])
