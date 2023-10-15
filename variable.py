d20a31549200ca1553af01099f35bf9ecb170967
xxsiddharth1 <siddharth2015sharma@gmail.com>
2023-10-15 20:12:20 +0530

    DSA

---
+++ variable.py
@@ -0,0 +1,16 @@
+
+
+
+
+#integer 
+icecream = 10
+print(type(icecream))
+#float
+cost = 12.5
+print(type(cost))
+#string
+name = "anuj"
+print(type(name))
+#booleans
+isanujboy = True
+print(type(isanujboy))
