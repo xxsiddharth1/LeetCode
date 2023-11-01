5f016719ebb458d97e97caba03728e9284cb2686
Siddharth Sharma <108428699+xxsiddharth1@users.noreply.github.com>
2023-11-01 19:02:33 +0530

    DSA

---
+++ longest_prefix.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        String prefix = strs[0];
+        for (String s : strs)
+            while (s.indexOf(prefix) != 0)
+                prefix = prefix.substring(0, prefix.length() - 1);
+        return prefix;
+    }
+}
